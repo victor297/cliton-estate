3f2ce9593c2f791557a679128fecc89247807a10
victor297 <clintondevs600@gmail.com>
2024-03-25 12:31:06 +0100

    address update2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
           <h5 className="footer-subtitle">The vision</h5>
           <p className="footer-text">
             <b>Address:</b> West link court 8, palm groove close, Iygokun GRA
-            Ibadan, Oyo
+            Ibadan, Oyo.
           </p>
           <p className="footer-text">
             <b>Mobile:</b> Lorem ipsum
